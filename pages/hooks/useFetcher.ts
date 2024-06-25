399df13b44254daf9f4b4730bc97cf9721f62c59
Ava Bakuie <avabakuie@Husseins-MacBook-Pro-2.fritz.box>
2024-06-25 14:11:48 +0200

    refactor useFetcher

--- pages/hooks/useFetcher.ts
+++ pages/hooks/useFetcher.ts
@@ -4,31 +4,19 @@ const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 const baseUrl = "https://api.thecatapi.com/v1";
 
 export const useFetcher = () => {
-    const [data, setData] = useState<any>(null); // Adjust type as per your API response structure
-    const [error, setError] = useState<Error | null>(null);
-    const [loading, setLoading] = useState<boolean>(false);
+  const fetchData = useCallback(async (endpoint: string, id?: string) => {
+    const fullUrl = id ? `${endpoint}/${id}` : endpoint;
+    const response = await fetch(`${baseUrl}/${fullUrl}`, {
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": apiKey ?? "",
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  }, []);
 
-    const fetchData = useCallback(async (endpoint: string, id?: string) => {
-        setLoading(true);
-        try {
-            const fullUrl = id ? `${endpoint}/${id}` : endpoint;
-            const response = await fetch(`${baseUrl}/${fullUrl}`, {
-                headers: {
-                    "Content-Type": "application/json",
-                    "x-api-key": apiKey ?? "",
-                },
-            });
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            const result = await response.json();
-            setData(result); // Assuming your API response is stored directly in state
-            setLoading(false);
-        } catch (error) {
-            setError(error as Error);
-            setLoading(false);
-        }
-    }, []);
-
-    return { data, error, loading, fetchData };
+  return { fetchData };
 };
