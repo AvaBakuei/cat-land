381bcd5eb8bf18ff187c24fea5af3cb7235aefc3
Ava Bakuie <avabakuie@Husseins-MacBook-Pro-2.fritz.box>
2024-06-28 23:15:08 +0200

    add new config for using plaieholder library for image placeholder, #1

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ import withPlaiceholder from "@plaiceholder/next";
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["images.unsplash.com"],
+    domains: ['cdn2.thecatapi.com'],
   },
 };
 
