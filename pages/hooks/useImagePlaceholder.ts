d7a27bdad4cf510253c2fa0110c0b0239cc96db7
Ava Bakuie <avabakuie@Husseins-MacBook-Pro-2.fritz.box>
2024-07-01 22:57:14 +0200

    refactor useImagePlaceholder, #1

--- pages/hooks/useImagePlaceholder.ts
+++ pages/hooks/useImagePlaceholder.ts
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
 
-export const useImagePlaceholder = (cardData: any) => {
+export const useImagePlaceholder = () => {
   const [base64, setBase64] = useState<string>("");
+  const src =
+    "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?q=80&w=2943&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
   useEffect(() => {
     const fetchPlaceholder = async () => {
-      if (!cardData || !cardData.imageUrl) return;
+      if (!src) return;
       try {
         const response = await fetch(
-          `/api/getImagePlaceholder?src=${encodeURIComponent(
-            cardData.imageUrl
-          )}`
+          `/api/getImagePlaceholder?src=${encodeURIComponent(src)}`
         );
         if (!response.ok) {
           throw new Error(`Failed to fetch placeholder: ${response.status}`);
@@ -23,7 +23,7 @@ export const useImagePlaceholder = (cardData: any) => {
     };
 
     fetchPlaceholder();
-  }, [cardData?.imageUrl]);
+  }, []);
 
   return { base64 };
 };
