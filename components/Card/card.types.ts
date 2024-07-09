dc3329e11bed143cdb67ae9fdb983c18e29ef925
Ava Bakuie <avabakuie@Husseins-MacBook-Pro-2.fritz.box>
2024-07-09 17:37:32 +0200

    add another card type, #3

--- components/Card/card.types.ts
+++ components/Card/card.types.ts
@@ -1,8 +1,22 @@
 export interface CardInterface {
-    id: string;
-    name: string;
-    origin: string;
-    imageUrl: string;
-    description: string;
-    href: string;
+  id: string;
+  name: string;
+  origin: string;
+  imageUrl: string;
+  description: string;
+  href: string;
+}
+
+export type HandleFavorite = (cardData: CardInterface) => void;
+
+export interface SingleCardProps {
+  cardData: CardInterface;
+  favorites: CardInterface[];
+  handleFavorite: HandleFavorite;
+}
+
+export interface CardListProps {
+  cardData: CardInterface[];
+  favorites: CardInterface[];
+  handleFavorite: HandleFavorite;
 }
