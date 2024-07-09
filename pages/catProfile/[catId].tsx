f3d3ec9a641fb3b81be9be6dc1e201fb043c4d6a
Ava Bakuie <avabakuie@Husseins-MacBook-Pro-2.fritz.box>
2024-07-09 12:21:31 +0200

    Use DetailSection in CatProfile page, #1

--- pages/catProfile/[catId].tsx
+++ pages/catProfile/[catId].tsx
@@ -4,87 +4,108 @@ import { fetchData } from "../api/fetchData";
 import { Loading } from "@/components/Loading";
 import Image from "next/image";
 import { useImagePlaceholder } from "../../components/hooks/useImagePlaceholder";
-import { Grid, Paper, Title, Text, useMantineTheme, Anchor } from "@mantine/core";
+import {
+  Grid,
+  Paper,
+  Title,
+  Text,
+  useMantineTheme,
+  Anchor,
+} from "@mantine/core";
 import styles from "./Cat.module.css";
+import DetailSection from "@/components/DetailSection/DetailSection";
 
 interface CatProfileProps {
-    catProfileData: CatProfileResponse | null;
-    error?: string;
+  catProfileData: CatProfileResponse | null;
+  error?: string;
 }
 
 const CatProfile: React.FC<CatProfileProps> = ({ catProfileData, error }) => {
-    const theme = useMantineTheme();
-    const { base64 } = useImagePlaceholder();
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
-    if (!catProfileData) {
-        return <Loading />;
-    }
+  const theme = useMantineTheme();
+  const { base64 } = useImagePlaceholder();
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+  if (!catProfileData) {
+    return <Loading />;
+  }
 
-    console.log("catProfileData", catProfileData);
+  return (
+    <Paper className={styles.catProfile} shadow="lg">
+      <Grid>
+        <Grid.Col span={{ sm: 6, md: 12, lg: 4 }}>
+          {catProfileData.imageUrl && base64 && (
+            <Image
+              src={catProfileData.imageUrl}
+              alt={catProfileData.name}
+              width={500}
+              height={500}
+              loading="lazy"
+              placeholder="blur"
+              blurDataURL={base64}
+              className={styles.catImage}
+            />
+          )}
+        </Grid.Col>
+        <Grid.Col
+          span={{ sm: 12, md: 12, lg: 8 }}
+          className={styles.catInformation}
+        >
+          <Title order={1} style={{ color: theme.colors.dark[5] }}>
+            {catProfileData.name}
+          </Title>
 
-    return (
-        <Paper className={styles.catProfile} shadow="lg">
-            <Grid>
-                <Grid.Col span={{ sm: 6, md: 12, lg: 4 }}>
-                    {catProfileData.imageUrl && base64 && (
-                        <Image
-                            src={catProfileData.imageUrl}
-                            alt={catProfileData.name}
-                            width={500}
-                            height={500}
-                            loading="lazy"
-                            placeholder="blur"
-                            blurDataURL={base64}
-                            className={styles.catImage}
-                        />
-                    )}
-                </Grid.Col>
-                <Grid.Col span={{ sm: 12, md: 12, lg: 8 }} className={styles.catInformation}>
-                    <Title order={1} style={{ color: theme.colors.dark[5] }}>{catProfileData.name}</Title>
-                    <div className={styles.data}>
-                        <Title style={{ color: theme.colors.dark[4] }} order={5}>Origin</Title>
-                        <Text style={{ color: theme.colors.dark[3] }}>{catProfileData.origin}</Text>
-                    </div>
-                    <div className={styles.data}>
-                        <Title style={{ color: theme.colors.dark[4] }} order={5}>Temperament</Title>
-                        <Text style={{ color: theme.colors.dark[3] }}>{catProfileData.temperament}</Text>
-                    </div>
-                    <div className={styles.data}>
-                        <Title style={{ color: theme.colors.dark[4] }} order={5}>Description</Title>
-                        <Text style={{ color: theme.colors.dark[3] }}>{catProfileData.description}</Text>
-                    </div>
-                    <Anchor href={catProfileData.wikipedia_url} target="_blank" className={styles.wikiLink}>
-                        Read More
-                    </Anchor>
-                </Grid.Col>
-            </Grid>
-        </Paper>
-    );
+          <DetailSection
+            className={styles.data}
+            title="Origin"
+            value={catProfileData.origin}
+          />
+
+          <DetailSection
+            className={styles.data}
+            title="Temperament"
+            value={catProfileData.temperament}
+          />
+
+          <DetailSection
+            className={styles.data}
+            title="Description"
+            value={catProfileData.description}
+          />
+          <Anchor
+            href={catProfileData.wikipedia_url}
+            target="_blank"
+            className={styles.wikiLink}
+          >
+            Read More
+          </Anchor>
+        </Grid.Col>
+      </Grid>
+    </Paper>
+  );
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-    const { catId } = context.params as { catId: string };
+  const { catId } = context.params as { catId: string };
 
-    try {
-        let catProfileData = await fetchData("breeds", catId);
-        const catImageId = catProfileData.reference_image_id;
-        const catImage = await fetchData("images", catImageId);
-        catProfileData = { ...catProfileData, imageUrl: catImage.url };
-        return {
-            props: {
-                catProfileData,
-            },
-        };
-    } catch (error: any) {
-        return {
-            props: {
-                catProfileData: null,
-                error: error.message,
-            },
-        };
-    }
+  try {
+    let catProfileData = await fetchData("breeds", catId);
+    const catImageId = catProfileData.reference_image_id;
+    const catImage = await fetchData("images", catImageId);
+    catProfileData = { ...catProfileData, imageUrl: catImage.url };
+    return {
+      props: {
+        catProfileData,
+      },
+    };
+  } catch (error: any) {
+    return {
+      props: {
+        catProfileData: null,
+        error: error.message,
+      },
+    };
+  }
 };
 
 export default CatProfile;
