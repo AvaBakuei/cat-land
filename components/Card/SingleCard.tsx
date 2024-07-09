db9bb152424826b6e142cd88b949a43cea039a4a
Ava Bakuie <avabakuie@Husseins-MacBook-Pro-2.fritz.box>
2024-07-09 11:59:26 +0200

    add favorite icon to SingleCard, #3

--- components/Card/SingleCard.tsx
+++ components/Card/SingleCard.tsx
@@ -1,17 +1,23 @@
 import { useState } from "react";
 import type { CardInterface } from "./card.types";
-import { Card, Text, Paper, Grid } from "@mantine/core";
+import { Card, Text, Paper, Grid, UnstyledButton, useMantineTheme } from "@mantine/core";
 import Image from "next/image";
 import styles from "./Card.module.css";
 import Link from "next/link";
 import { useImagePlaceholder } from "@/components/hooks/useImagePlaceholder";
+import { IconHeart, IconHeartFilled } from "@tabler/icons-react";
 
 export const SingleCard = ({ cardData }: { cardData: CardInterface }) => {
+  const theme = useMantineTheme();
   const [hover, setHover] = useState(false);
+  const [isFave, setIsFave] = useState(false);
   const { base64 } = useImagePlaceholder();
 
   const handleMouseEnter = () => setHover(true);
   const handleMouseLeave = () => setHover(false);
+  const handleFavorite = () => {
+    setIsFave(!isFave)
+  }
 
   return (
     <Grid.Col span={3} className={styles.cardGrid}>
@@ -22,10 +28,10 @@ export const SingleCard = ({ cardData }: { cardData: CardInterface }) => {
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       >
-        <Link
-          href={`/catProfile/${cardData.id}`}
-          className={`${styles.imageCard} ${hover ? styles.hovered : ""}`}
-        >
+        <div className={styles.imageCard} >
+          <UnstyledButton className={styles.fav} onClick={handleFavorite} style={{ backgroundColor: isFave ? theme.colors.pink[0] : theme.colors.gray[0] }}>
+            {isFave ? <IconHeartFilled color={theme.colors.pink[6]} /> : <IconHeart stroke={2} color={theme.colors.gray[5]} />}
+          </UnstyledButton>
           {base64 && (
             <Image
               src={cardData.imageUrl}
@@ -37,13 +43,19 @@ export const SingleCard = ({ cardData }: { cardData: CardInterface }) => {
               blurDataURL={base64}
             />
           )}
+        </div>
 
-          <Paper shadow="xs" className={styles.name}>
-            <Text size="sm" ta="center" fw={500} c="black">
+        <Paper shadow="xs" className={styles.name}>
+          <Link
+            href={`/catProfile/${cardData.id}`}
+            className={`${hover ? styles.hovered : ""}`}
+            style={{ textDecoration: "none" }}
+          >
+            <Text size="sm" ta="center" fw={500} c={theme.colors.dark[4]} >
               {cardData.name}
             </Text>
-          </Paper>
-        </Link>
+          </Link>
+        </Paper>
       </Card>
     </Grid.Col>
   );
