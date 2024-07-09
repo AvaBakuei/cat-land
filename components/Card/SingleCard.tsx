e94a9c4be9c5e2b52b40ebe84d7a24d331bb482b
Ava Bakuie <avabakuie@Husseins-MacBook-Pro-2.fritz.box>
2024-07-09 17:38:46 +0200

    add favorite button in SingleCard component, #3

--- components/Card/SingleCard.tsx
+++ components/Card/SingleCard.tsx
@@ -1,23 +1,41 @@
-import { useState } from "react";
-import type { CardInterface } from "./card.types";
-import { Card, Text, Paper, Grid, UnstyledButton, useMantineTheme } from "@mantine/core";
+import { useEffect, useState } from "react";
+import type { CardInterface, SingleCardProps } from "./card.types";
+import {
+  Card,
+  Text,
+  Paper,
+  Grid,
+  UnstyledButton,
+  useMantineTheme,
+} from "@mantine/core";
 import Image from "next/image";
 import styles from "./Card.module.css";
 import Link from "next/link";
 import { useImagePlaceholder } from "@/components/hooks/useImagePlaceholder";
 import { IconHeart, IconHeartFilled } from "@tabler/icons-react";
 
-export const SingleCard = ({ cardData }: { cardData: CardInterface }) => {
+export const SingleCard: React.FC<SingleCardProps> = ({
+  cardData,
+  favorites,
+  handleFavorite,
+}) => {
   const theme = useMantineTheme();
   const [hover, setHover] = useState(false);
-  const [isFave, setIsFave] = useState(false);
+  const [isFav, setIsFav] = useState(false);
+
   const { base64 } = useImagePlaceholder();
 
   const handleMouseEnter = () => setHover(true);
   const handleMouseLeave = () => setHover(false);
-  const handleFavorite = () => {
-    setIsFave(!isFave)
-  }
+  const handleClick = (cardData: CardInterface) => {
+    handleFavorite(cardData);
+    setIsFav(!isFav);
+  };
+
+  useEffect(() => {
+    const isFavorite = favorites.some((fav) => fav.id === cardData.id);
+    setIsFav(isFavorite);
+  }, [favorites]);
 
   return (
     <Grid.Col span={3} className={styles.cardGrid}>
@@ -28,9 +46,21 @@ export const SingleCard = ({ cardData }: { cardData: CardInterface }) => {
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       >
-        <div className={styles.imageCard} >
-          <UnstyledButton className={styles.fav} onClick={handleFavorite} style={{ backgroundColor: isFave ? theme.colors.pink[0] : theme.colors.gray[0] }}>
-            {isFave ? <IconHeartFilled color={theme.colors.pink[6]} /> : <IconHeart stroke={2} color={theme.colors.gray[5]} />}
+        <div className={styles.imageCard}>
+          <UnstyledButton
+            className={styles.fav}
+            onClick={() => handleClick(cardData)}
+            style={{
+              backgroundColor: isFav
+                ? theme.colors.pink[0]
+                : theme.colors.gray[0],
+            }}
+          >
+            {isFav ? (
+              <IconHeartFilled color={theme.colors.pink[6]} />
+            ) : (
+              <IconHeart stroke={2} color={theme.colors.gray[5]} />
+            )}
           </UnstyledButton>
           {base64 && (
             <Image
@@ -51,7 +81,7 @@ export const SingleCard = ({ cardData }: { cardData: CardInterface }) => {
             className={`${hover ? styles.hovered : ""}`}
             style={{ textDecoration: "none" }}
           >
-            <Text size="sm" ta="center" fw={500} c={theme.colors.dark[4]} >
+            <Text size="sm" ta="center" fw={500} c={theme.colors.dark[4]}>
               {cardData.name}
             </Text>
           </Link>
