5322e2ac35571d42339ee0f13bb9ce568eb96ead
Ava Bakuie <avabakuie@Husseins-MacBook-Pro-2.fritz.box>
2024-07-10 22:04:29 +0200

    change the size of cat animation, #3

--- components/EmptyBlock.tsx
+++ components/EmptyBlock.tsx
@@ -2,39 +2,39 @@ import Lottie from "lottie-react";
 import catAnimation from "../public/catAnimation.json";
 import { Center, Text, useMantineTheme } from "@mantine/core";
 interface EmptyBlockProps {
-  title: string;
+    title: string;
 }
 
 export const EmptyBlock: React.FC<EmptyBlockProps> = ({ title }) => {
-  const theme = useMantineTheme();
-  return (
-    <Center
-      style={{
-        position: "relative",
-        width: "100%",
-        height: "calc(100vh - 82px)",
-      }}
-    >
-      <Text
-        style={{
-          position: "absolute",
-          top: "125px",
-          color: theme.colors.gray[6],
-          fontSize: "2.25rem",
-        }}
-      >
-        {title}
-      </Text>
-      <Lottie
-        animationData={catAnimation}
-        loop={false}
-        style={{
-          position: "absolute",
-          bottom: "0",
-          margin: "0 auto",
-          width: "650px",
-        }}
-      />
-    </Center>
-  );
+    const theme = useMantineTheme();
+    return (
+        <Center
+            style={{
+                position: "relative",
+                width: "100%",
+                height: "calc(100vh - 82px)",
+            }}
+        >
+            <Text
+                style={{
+                    position: "absolute",
+                    top: "125px",
+                    color: theme.colors.gray[6],
+                    fontSize: "2.25rem",
+                }}
+            >
+                {title}
+            </Text>
+            <Lottie
+                animationData={catAnimation}
+                loop={false}
+                style={{
+                    position: "absolute",
+                    bottom: "0",
+                    margin: "0 auto",
+                    width: "550px",
+                }}
+            />
+        </Center>
+    );
 };
