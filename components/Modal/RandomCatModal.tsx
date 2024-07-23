78144b95eaac351aa8e2c1ada9a6dd95b0318a72
Ava Bakuie <avabakuie@Husseins-MacBook-Pro-2.fritz.box>
2024-07-23 22:24:36 +0200

    use useStyles component in the RandomCatModal, #13

--- components/Modal/RandomCatModal.tsx
+++ components/Modal/RandomCatModal.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
 import { useDisclosure } from "@mantine/hooks";
-import { Modal, Button, PinInput } from "@mantine/core";
-import { IconCat } from "@tabler/icons-react";
+import { Modal, Text, Button, PinInput, useMantineTheme } from "@mantine/core";
+import { IconPaw } from "@tabler/icons-react";
 import { useImperativeHandle, forwardRef, useState } from "react";
+import { useImagePlaceholder } from "@/components/common/hooks/useImagePlaceholder";
+import useStyles from "../common/hooks/useStyles";
 
 interface RandomCatModalProps {
     title: string;
@@ -9,6 +12,7 @@ interface RandomCatModalProps {
     buttonTitle: string;
     isVerify: boolean;
     handleVerifyCode: (value: string) => void;
+    catSrc: string;
 }
 
 export interface RandomCatModalRef {
@@ -18,34 +22,64 @@ export interface RandomCatModalRef {
 export const RandomCatModal = forwardRef<
     RandomCatModalRef,
     RandomCatModalProps
->(({ title, codeLength, buttonTitle, isVerify, handleVerifyCode }, ref) => {
-    const [opened, { open, close }] = useDisclosure(false);
-    const [value, setValue] = useState<string>("");
+>(
+    (
+        { title, codeLength, buttonTitle, isVerify, handleVerifyCode, catSrc },
+        ref
+    ) => {
+        const classes = useStyles({ theme: useMantineTheme() });
+        const [opened, { open, close }] = useDisclosure(false);
+        const [value, setValue] = useState<string>("");
+        const { base64 } = useImagePlaceholder();
 
-    useImperativeHandle(ref, () => ({
-        openModal: open,
-    }));
-    const onChange = (e: string) => {
-        setValue(e);
-    };
+        useImperativeHandle(ref, () => ({
+            openModal: open,
+        }));
+        const onChange = (e: string) => {
+            setValue(e);
+        };
 
-    const handleModal = () => {
-        handleVerifyCode(value);
-    };
+        const handleModal = () => {
+            handleVerifyCode(value);
+        };
 
-    return (
-        <>
-            <Modal opened={opened} onClose={close} title={<IconCat stroke={2} />}>
-                {!isVerify ? (
-                    <>
-                        <div>{title}</div>
-                        <PinInput length={codeLength} onChange={onChange} />
-                        <Button onClick={handleModal}>{buttonTitle}</Button>
-                    </>
-                ) : (
-                    <div>Show a random cat picture</div>
-                )}
-            </Modal>
-        </>
-    );
-});
+        return (
+            <>
+                <Modal
+                    opened={opened}
+                    onClose={close}
+                    title={<IconPaw stroke={2} className={classes.modalIcon} />}
+                    className={classes.modal}
+                >
+                    {!isVerify ? (
+                        <>
+                            <Text size="xl" fw="500" className={classes.modalTitle}>
+                                {title}
+                            </Text>
+                            <PinInput
+                                className={classes.pinStyle}
+                                length={codeLength}
+                                onChange={onChange}
+                            />
+                            <Button onClick={handleModal} className={classes.modalBtn}>
+                                {buttonTitle}
+                            </Button>
+                        </>
+                    ) : (
+                        <div className={classes.modalBody}>
+                            <Image
+                                className={classes.modalImage}
+                                src={catSrc}
+                                alt="Cat Image"
+                                fill
+                                loading="lazy"
+                                placeholder="blur"
+                                blurDataURL={base64}
+                            />
+                        </div>
+                    )}
+                </Modal>
+            </>
+        );
+    }
+);
