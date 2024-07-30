2204f3cb6fa8e22399c0f901726a112f443dfefc
Ava Bakuie <avabakuie@Husseins-MacBook-Pro-2.fritz.box>
2024-07-30 21:15:53 +0200

    refactor RandomCatModal, added some functionality and change the concept, #18

--- components/Modal/RandomCatModal.tsx
+++ components/Modal/RandomCatModal.tsx
@@ -1,35 +1,44 @@
 import Image from "next/image";
-import { useDisclosure } from "@mantine/hooks";
-import { Modal, Text, Button, PinInput, useMantineTheme } from "@mantine/core";
-import { IconPaw } from "@tabler/icons-react";
-import { useImperativeHandle, forwardRef, useState } from "react";
+import {
+    Modal,
+    Text,
+    Button,
+    PinInput,
+    useMantineTheme,
+    Center,
+    Anchor,
+    Paper,
+    Title,
+} from "@mantine/core";
+import { useState } from "react";
 import { useImagePlaceholder } from "@/components/common/hooks/useImagePlaceholder";
 import useStyles from "../common/hooks/useStyles";
+import { CardInterface } from "../common/types/card.types";
+import Link from "next/link";
 
 interface RandomCatModalProps {
-    title: string;
-    codeLength: number;
-    buttonTitle: string;
+    catInfo: CardInterface | undefined;
     isVerify: boolean;
     handleVerifyCode: (value: string) => void;
-    catSrc: string;
     opened: boolean;
     onClose: () => void;
+    pinError: boolean;
 }
 
 export const RandomCatModal: React.FC<RandomCatModalProps> = ({
-    title,
-    codeLength,
-    buttonTitle,
+    catInfo,
     isVerify,
     handleVerifyCode,
-    catSrc,
     opened,
     onClose,
+    pinError,
 }) => {
     const classes = useStyles({ theme: useMantineTheme() });
     const [value, setValue] = useState<string>("");
     const { base64 } = useImagePlaceholder();
+    console.log("pinerror", pinError);
+
+    const catBreedStr = catInfo?.name.split(" ").join("");
 
     const onChange = (e: string) => {
         setValue(e);
@@ -41,40 +50,75 @@ export const RandomCatModal: React.FC<RandomCatModalProps> = ({
 
     return (
         <>
-            <Modal
-                opened={opened}
-                onClose={onClose}
-                title={<IconPaw stroke={2} className={classes.modalIcon} />}
-                className={classes.modal}
-            >
-                {!isVerify ? (
-                    <>
-                        <Text size="xl" fw="500" className={classes.modalTitle}>
-                            {title}
+            {catInfo && (
+                <Modal
+                    size="lg"
+                    opened={opened}
+                    onClose={onClose}
+                    title={
+                        <Text ta="center" size="lg" fw="500" className={classes.modalTitle}>
+                            Guess the Cat Breed!
                         </Text>
-                        <PinInput
-                            className={classes.pinStyle}
-                            length={codeLength}
-                            onChange={onChange}
-                        />
-                        <Button onClick={handleModal} className={classes.modalBtn}>
-                            {buttonTitle}
-                        </Button>
-                    </>
-                ) : (
-                    <div className={classes.modalBody}>
+                    }
+                    className={classes.modal}
+                >
+                    <Center className={classes.modalBody}>
                         <Image
                             className={classes.modalImage}
-                            src={catSrc}
+                            src={catInfo?.imageUrl ?? base64}
                             alt="Cat Image"
                             fill
                             loading="lazy"
                             placeholder="blur"
                             blurDataURL={base64}
                         />
-                    </div>
-                )}
-            </Modal>
+                    </Center>
+                    {!isVerify ? (
+                        <>
+                            <Text ta="center" size="md" className={classes.modalText}>
+                                Welcome to our daily cat challenge! 🐱
+                                <br />
+                                Can you guess the breed of this adorable cat?
+                                <br />
+                                Enter your guess in the field below and see if you’re right!
+                            </Text>
+                            <PinInput
+                                className={classes.pinStyle}
+                                length={catBreedStr?.length}
+                                onChange={onChange}
+                                placeholder=""
+                                size="xs"
+                                error={pinError}
+                            />
+                            <Center>
+                                <Button onClick={handleModal} className={classes.modalBtn}>
+                                    Verify Code
+                                </Button>
+                            </Center>
+                        </>
+                    ) : (
+                        <>
+                            <Text ta="center" size="md" className={classes.modalText}>
+                                🎉 Congratulations! 🎉
+                                <br />
+                                You guessed it right! This cat is indeed a{" "}
+                                <Title order={6}>{catInfo.name}</Title>
+                                Click below to learn more about this breed.
+                            </Text>
+                            <Center>
+                                <Link
+                                    href={`/catProfile/${catInfo.id}`}
+                                    className={classes.modalLink}
+                                >
+                                    <Button onClick={handleModal} className={classes.modalBtn}>
+                                        Go to Cat Details
+                                    </Button>
+                                </Link>
+                            </Center>
+                        </>
+                    )}
+                </Modal>
+            )}
         </>
     );
 };
