c9b917e886d5f32a829b6c8038165333f4b9e7e4
Ava Bakuie <avabakuie@Husseins-MacBook-Pro-2.fritz.box>
2024-08-01 17:38:42 +0200

    refactor app page and remove useless code, #22

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,16 @@
 import "@mantine/core/styles.css";
 import type { AppProps } from "next/app";
 import { createTheme, MantineProvider } from "@mantine/core";
-import { QueryClient } from "@tanstack/react-query";
-import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
-import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
-import localForage from "localforage";
 import { Header } from "@/components/Header/Header";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      gcTime: 1000 * 60 * 60 * 24, // 24 hours
-    },
-  },
-});
-
-const asyncStoragePersister = createAsyncStoragePersister({
-  storage: localForage,
-});
 
 export default function App({ Component, pageProps }: AppProps) {
   const theme = createTheme({});
   return (
-    <PersistQueryClientProvider
-      client={queryClient}
-      persistOptions={{ persister: asyncStoragePersister }}
-    >
-      <MantineProvider theme={theme}>
-        <div style={{ padding: "2rem 6rem"}}>
-          <Header />
-          <Component {...pageProps} />
-        </div>
-      </MantineProvider>
-    </PersistQueryClientProvider>
+    <MantineProvider theme={theme}>
+      <div style={{ padding: "2rem 6rem" }}>
+        <Header />
+        <Component {...pageProps} />
+      </div>
+    </MantineProvider>
   );
 }
