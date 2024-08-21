5a0a4c75acc4b853c6b24b746d5dc60f32ce6edd
Ava Bakuie <avabakuie@Husseins-MBP-2.fritz.box>
2024-08-21 21:49:57 +0200

    add styles to _app.tsx file and write mediaqueri for it, #24

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,13 @@ import "@mantine/core/styles.css";
 import type { AppProps } from "next/app";
 import { createTheme, MantineProvider } from "@mantine/core";
 import { Header } from "@/components/Header/Header";
+import styles from "../styles/Home.module.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   const theme = createTheme({});
   return (
     <MantineProvider theme={theme}>
-      <div style={{ padding: "2rem 6rem" }}>
+      <div className={styles.app}>
         <Header />
         <Component {...pageProps} />
       </div>
